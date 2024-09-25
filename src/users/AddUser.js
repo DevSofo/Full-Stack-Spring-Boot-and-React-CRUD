9bbcfc87d5880a2b28cbbc866fa60e16d0b7360f
Sofoklis <sofoklismaniatopoulos@gmail.com>
2024-09-25 15:54:47 +0300

    Store information inside useState

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddUser() {
+
+    const [user,setUser]=useState({
+      name:"",
+      username:"",
+      email:""
+    });
+
+    const{name,username,email}=user;
+
+    const onInputChange=(e)=>{
+      setUser({...user,[e.target.name]:e.target.value});
+    };
+
+
   return <div className='container'>
     <div className='row'>
       <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
@@ -9,7 +23,13 @@ export default function AddUser() {
             <label htmlFor='Name' className='form-laber'>
               Name
             </label>
-            <input type={'text'} className='form-control'placeholder='Enter your name' name='name'/>
+            <input 
+            type={'text'} 
+            className='form-control'
+            placeholder='Enter your name' 
+            name='name'  
+            value={name} 
+            onChange={(e)=>onInputChange(e)}/>
           </div>
       </div>
       <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
@@ -18,7 +38,13 @@ export default function AddUser() {
             <label htmlFor='Username' className='form-laber'>
               Username
             </label>
-            <input type={'text'} className='form-control'placeholder='Enter your Username' name='username'/>
+            <input 
+            type={'text'} 
+            className='form-control'
+            placeholder='Enter your Username' 
+            name='username' 
+            value={username}
+            onChange={(e)=>onInputChange(e)}/>
           </div>
       </div>
       <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
@@ -27,7 +53,13 @@ export default function AddUser() {
             <label htmlFor='Email' className='form-laber'>
               Email
             </label>
-            <input type={'text'} className='form-control'placeholder='Enter your email adress' name='email'/>
+            <input 
+            type={'text'} 
+            className='form-control'
+            placeholder='Enter your email adress' 
+            name='email' 
+            value={email}
+            onChange={(e)=>onInputChange(e)}/>
           </div>
           <button type='submit' className='btn btn-outline-primary'>
               Submit
