34872619ded331ecdb9f654396a6158a043dbe13
Sofoklis <sofoklismaniatopoulos@gmail.com>
2024-09-26 16:12:32 +0300

    Custom Exception Handling

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,75 +1,80 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function AddUser() {
 
-    const [user,setUser]=useState({
-      name:"",
-      username:"",
-      email:""
-    });
+  let navigate=useNavigate();
 
-    const{name,username,email}=user;
+  const [user,setUser]=useState({
+    name:"",
+    username:"",
+    email:""
+  });
 
-    const onInputChange=(e)=>{
-      setUser({...user,[e.target.name]:e.target.value});
-    };
+  const{name,username,email}=user;
 
+  const onInputChange=(e)=>{
+      setUser({...user,[e.target.name]:e.target.value})
+  };
 
-  return <div className='container'>
+  const onSubmit=async(e)=>{
+    e.preventDefault();
+    await axios.post("http://localhost:8080/user",user)
+    navigate("/")
+  }
+
+  return (
+  <div className='container'>
     <div className='row'>
       <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Register User</h2>
+          <form onSubmit={(e)=>onSubmit(e)}>
           <div className='mb-3'>
-            <label htmlFor='Name' className='form-laber'>
-              Name
-            </label>
-            <input 
-            type={'text'} 
-            className='form-control'
-            placeholder='Enter your name' 
-            name='name'  
-            value={name} 
-            onChange={(e)=>onInputChange(e)}/>
-          </div>
-      </div>
-      <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
-          <h2 className='text-center m-4'>Register User</h2>
+              <label htmlFor='Name' className='form-label'>
+                Name
+              </label>
+              <input
+              type={"text"}
+              className='form-control'
+              placeholder='Enter your name'
+              name="name"
+              value={name}
+              onChange={(e)=>onInputChange(e)}/>
+          </div> 
           <div className='mb-3'>
-            <label htmlFor='Username' className='form-laber'>
-              Username
-            </label>
-            <input 
-            type={'text'} 
-            className='form-control'
-            placeholder='Enter your Username' 
-            name='username' 
-            value={username}
-            onChange={(e)=>onInputChange(e)}/>
-          </div>
-      </div>
-      <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
-          <h2 className='text-center m-4'>Register User</h2>
+              <label htmlFor='Username' className='form-label'>
+                Username
+              </label>
+              <input
+              type={"text"}
+              className='form-control'
+              placeholder='Enter your username'
+              name='username'
+              value={username}
+              onChange={(e)=>onInputChange(e)}/>
+          </div> 
           <div className='mb-3'>
-            <label htmlFor='Email' className='form-laber'>
-              Email
-            </label>
-            <input 
-            type={'text'} 
-            className='form-control'
-            placeholder='Enter your email adress' 
-            name='email' 
-            value={email}
-            onChange={(e)=>onInputChange(e)}/>
+              <label htmlFor='Email' className='form-label'>
+                E-mail
+              </label>
+              <input
+              type={"text"}
+              className='form-control'
+              placeholder='Enter your E-mail'
+              name='email'
+              value={email}
+              onChange={(e)=>onInputChange(e)}/>
+              <button type='submit' className='btn btn-outline-primary'>
+                Submit
+              </button>
+              <Link className='btn btn-outline-danger' to="/">
+                Cancel
+              </Link>
           </div>
-          <button type='submit' className='btn btn-outline-primary'>
-              Submit
-          </button>
-          <button type='submit' className='btn btn-outline-danger'>
-              Cancel
-          </button>
+          </form> 
         </div>
-    </div>
-      
-  </div>;
-  
+    </div>   
+  </div>
+  );  
 }
